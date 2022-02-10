923a3800192aa4d497e7c92cab5fae722ec211e6
memokkkk <2301789566@qq.com>
2022-02-10 12:54:40 -0800

    Edited html and py files

--- Webapp/app.py
+++ Webapp/app.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route("/")
+def main():
+    return render_template("main_page.html")
 
+@app.route("/other_page1/")
+def view():
+    return render_template("other_page1.html")
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/other_page2/")
+def view():
+    return render_template("other_page2.html")
+
+@app.route("/result_page/")
+def view():
+    return render_template("result_page1.html")
