2c4d443d1d64ac4f66b0611ff58472ed18981b44
jeff1hwang <hzf520520@gmail.com>
2022-03-03 16:22:49 -0800

    Saving Best Model to file

--- Webapp/app.py
+++ Webapp/app.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 def main():
     return render_template("main_page.html")
 
-@app.route("/other_page1/")
-def view():
-    return render_template("other_page1.html")
+# @app.route("/other_page1/")
+# def view():
+#     return render_template("other_page1.html")
 
-@app.route("/other_page2/")
-def view():
-    return render_template("other_page2.html")
+# @app.route("/other_page2/")
+# def view():
+#     return render_template("other_page2.html")
 
-@app.route("/result_page/")
-def view():
-    return render_template("result_page1.html")
+# @app.route("/result_page/")
+# def view():
+#     return render_template("result_page1.html")
